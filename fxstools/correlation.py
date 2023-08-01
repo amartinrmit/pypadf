fix logging of qmax to parameter log; fix qmax calculation
Former-commit-id: b30bc7dbd9739abcfdf1a0a5c1e577423ed27db3

fxstools/correlation.py
@@ -176,6 +176,7 @@ class correlation:
 
         self.wl = wl
         self.pixel_width = pw
+        self.pwsave = self.pixel_width
         self.detector_z = dz
         self.path = path
         self.tag = tag
@@ -243,8 +244,10 @@ class correlation:
         self.inputpath = "None"
         self.inputtag = "None"
        
-        self.ac = crtls.angular_correlation() 
-        self.qbins = self.ac.qbins( self.nx//2, self.nx//2, self.detector_z, 
+        self.ac = crtls.angular_correlation()
+        self.qpmax = self.maxpixeldist()  
+        self.qmax_calc()
+        self.qbins = self.ac.qbins( self.nx//2, self.qpmax, self.detector_z, 
                                          self.wl, self.pixel_width )
         #print(self.cx, self.cy,"check in init")
 
@@ -997,3 +1000,24 @@ class correlation:
         if corrtype == 'difference':
             self.bg_estimate = False
             self.diffcorrflag = True
+ 
+    # set the pixel length to max pixel radius used
+    def maxpixeldist( self ):
+        if self.crop_flag:
+            mpdist = np.min([self.nxcrop,self.nycrop])/2
+        else:
+            mpdist = np.min([self.nx,self.ny])/2
+        return mpdist
+
+    def qmax_calc(self):
+        """Calculate the maximum q value at the edge of the detector (or diffraction pattern)
+           
+            Assumes diffraction pattern is centred in the array
+        """
+        thmax = np.arctan(self.qpmax*self.pwsave/self.detector_z)
+        self.qmax = (2/self.wl) * np.sin( thmax/2.0 )
+
+    def append_qmax_to_parameter_log(self, fname):
+        with open(fname, 'a') as f:
+            f.write('qmax = '+str(self.qmax))
+            f.close()

params/paramsCORR.py
@@ -287,12 +287,12 @@ class paramsCORR(params):
         print("config file name:", abspath )
         self.check_corrtype()
         self.checkpaths()
-        self.qmax_calc()
-        self.add_parameter( "qmax", self.qmax, cmdline="--dontuse", 
-                            help="dont input qmax is calculated by the code", 
-                            nargs=1, header="DONTUSE", pathflag=False )
+        #self.qmax_calc()
         outname = self.makefname( self.outpath, self.tag, "_difftocorr_parameter_log", ".txt")
         self.write_params_to_file( outname )
+        self.add_parameter( "qmax", 1.0, cmdline="--dontuse", 
+                            help="dont input qmax is calculated by the code", 
+                            nargs=1, header="DONTUSE", pathflag=False )
 
     def load_flist_from_samplepath(self):
         """List the diffraction files in the samplepath
@@ -315,13 +315,13 @@ class paramsCORR(params):
                 print("Check the sample path - not a valid directory, \
                       filelist or regular expression")
 
-    def qmax_calc(self):
-        """Calculate the maximum q value at the edge of the detector (or diffraction pattern)
-           
-            Assumes diffraction pattern is centred in the array
-        """
-        thmax = np.arctan( (self.nq)*self.pw/self.dz)
-        self.qmax = (2/self.wl) * np.sin( thmax/2.0 )
+    #def qmax_calc(self):
+    #    """Calculate the maximum q value at the edge of the detector (or diffraction pattern)
+    #       
+    #        Assumes diffraction pattern is centred in the array
+    #    """
+    #    thmax = np.arctan( (min([self.nxcrop,self.nycrop]]))*self.pw/self.dz)
+    #    self.qmax = (2/self.wl) * np.sin( thmax/2.0 )
 
     def check_corrtype(self):
         self.corrtype.lower()